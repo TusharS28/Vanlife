90e9f29a04be488d8beda0046948e27becfcfe1c
TusharS28 <tusharsingh2822@gmail.com>
2023-11-24 14:22:56 +0530

    vans-page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import React from 'react';
-import About from './pages/About/About';
 import { BrowserRouter, Routes, Route,Link } from "react-router-dom"
+import "./server.js"
+import About from './pages/About/About';
 import Home from './pages/Home/Home';
-import "server.js"
+import Vans from './pages/Vans/Vans';
+import Vandetail from './pages/Vans/Vandetail.jsx';
 
 function App(){
     return (
@@ -18,6 +20,8 @@ function App(){
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans/>} />
+          <Route path='/vans/:id' element={<Vandetail/>} />
         </Routes>
       </BrowserRouter>
     );
