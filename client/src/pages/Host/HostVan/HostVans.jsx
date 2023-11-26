9a808c3d2980ed826848710464b3bf07995d6b57
TusharS28 <tusharsingh2822@gmail.com>
2023-11-26 13:22:15 +0530

    change

--- client/src/pages/Host/HostVan/HostVans.jsx
+++ client/src/pages/Host/HostVan/HostVans.jsx
@@ -1,14 +1,15 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import "./hostvan.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./hostvan.css";
+
 export default function HostVans() {
-    const [vans, setVans] = React.useState([])
+    const [vans, setVans] = React.useState([]);
 
     React.useEffect(() => {
         fetch("/api/host/vans")
             .then(res => res.json())
-            .then(data => setVans(data.vans))
-    }, [])
+            .then(data => setVans(data.vans));
+    }, []);
 
     const hostVansEls = vans.map(van => (
         <Link
@@ -16,31 +17,26 @@ export default function HostVans() {
             key={van.id}
             className="host-van-link-wrapper"
         >
-            <div className="host-van-single" key={van.id}>
-                <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+            <div className="host-van-single">
+                <img src={van.imageUrl /* eslint-disable jsx-a11y/img-redundant-alt */} alt={'' /* eslint-disable jsx-a11y/img-redundant-alt */}/>
                 <div className="host-van-info">
                     <h3>{van.name}</h3>
                     <p>${van.price}/day</p>
                 </div>
             </div>
         </Link>
-    ))
+    ));
 
     return (
         <section>
             <h1 className="host-vans-title">Your listed vans</h1>
             <div className="host-vans-list">
-                {
-                    vans.length > 0 ? (
-                        <section>
-                            {hostVansEls}
-                        </section>
-
-                    ) : (
-                            <h2>Loading...</h2>
-                        )
-                }
+                {vans.length > 0 ? (
+                    <section>{hostVansEls}</section>
+                ) : (
+                    <h2>Loading...</h2>
+                )}
             </div>
         </section>
-    )
+    );
 }
